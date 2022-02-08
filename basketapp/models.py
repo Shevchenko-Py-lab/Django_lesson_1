f3e823c27ac50716aa550cd58bf613cb95f89f70
Shevchenko-Py-lab <shevchenko.logist@mail.ru>
2022-02-08 19:53:38 +0300

    check_Django2_lesson_7

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.utils.functional import cached_property
 
 from mainapp.models import Product
 
@@ -26,6 +27,18 @@ class Basket(models.Model):
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
 
     add_datetime = models.DateTimeField(verbose_name='время', auto_now_add=True)
+
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
+
+    def get_total_quantity(self):
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.quantity, _items)))
+
+    def get_total_cost(self):
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.product_cost, _items)))
 
     @staticmethod
     def get_item(pk):
