a682459496285cc181ba68eb7480581296f8561a
Shevchenko-Py-lab <shevchenko.logist@mail.ru>
2022-02-10 00:07:55 +0300

    check_Django2_lesson_8

--- basketapp/views.py
+++ basketapp/views.py
@@ -33,15 +33,15 @@ def basket_add(request, pk):
     if not basket:
         basket = Basket(user=request.user, product=product)
 
-    # basket.quantity += 1
-    # basket.save()
-
-    old_basket_item = Basket.get_product(user=request.user, product=product)
-    if old_basket_item:
-        old_basket_item[0].quantity += 1
-        old_basket_item[0].save()
-    else:
-        old_basket_item[0].quantity = F('quantity') + 1
+    basket.quantity += 1
+    basket.save()
+    #
+    # old_basket_item = Basket.get_product(user=request.user, product=product)
+    # if old_basket_item:
+    #     old_basket_item[0].quantity += 1
+    #     old_basket_item[0].save()
+    # else:
+    #     old_basket_item[0].quantity = F('quantity') + 1
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
